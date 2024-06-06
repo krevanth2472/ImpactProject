452f3fdbee983d5bd92f08fb87841f28deae5d58
krevanth2472 <krevanthreddy1234@gmail.com>
2024-06-06 22:07:09 +0530

    Footer Changed

--- Appoitment/src/Components/Footer.jsx
+++ Appoitment/src/Components/Footer.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-      
-    </div>
+    <footer>
+        <div className="glassdiv2">
+            <div className="logo">
+                <span>Medi</span><span style={{color: '#C42121'}}>Go</span>
+            </div>
+            <div className="slogan" style={{fontSize: '35px', color: '#aaaaaa'}}>
+                Your Health our Goal
+            </div>
+            <hr color='#aaaaaa' style={{marginTop: '30px',marginBottom: '30px',height: '1px',border: 'none'}}/>
+            <div style={{width: '60%',display: 'flex', justifyContent: 'spaceBetween',color: '#767676',fontSize: '20px',fontWeight: '500'}}>
+                <div>All Right Reserved 2024</div>
+                <div>Terms and Contditions</div>
+                <div>Privacy Policy</div>
+            </div>
+        </div>
+    </footer>
   )
 }
